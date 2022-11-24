274bb9433e3d88fecfdcd4f97a02a09057ac97b8
Fubyx <fabireif11@gmail.com>
2022-11-24 08:53:21 +0100

    //Starting screen

--- src/main/java/com/example/_06_tik_tak_toe/HelloController.java
+++ src/main/java/com/example/_06_tik_tak_toe/HelloController.java
@@ -4,11 +4,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField txt;
+    @FXML
+    private Label label;
     private boolean turn = false;
+
+    @FXML
+    protected void startButtonClicked(){
+        try {
+            int x = Integer.parseInt(txt.getText());
+            System.out.println(x);
+        }catch (NumberFormatException e){
+            label.setText("Enter a valid Number");
+        }
+    }
+
 
     @FXML
     protected void clicked(ActionEvent e) {
