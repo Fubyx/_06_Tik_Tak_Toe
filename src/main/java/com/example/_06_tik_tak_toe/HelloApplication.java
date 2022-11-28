84d918a3ec1a73b0fe464770f312cc8d42c9f612
TheD4rkCoder <david2mairhofer@gmail.com>
2022-11-28 17:43:42 +0100

    buttons

--- src/main/java/com/example/_06_tik_tak_toe/HelloApplication.java
+++ src/main/java/com/example/_06_tik_tak_toe/HelloApplication.java
@@ -3,16 +3,13 @@ package com.example._06_tik_tak_toe;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,16 +18,90 @@ public class HelloApplication extends Application {
 
 
     private boolean turnOfPlayer1 = true;
+    private Button[][] field;
+    private int width;
+    private boolean stop = false;
 
     @Override
     public void start(Stage stage) throws IOException {
+        Label label = new Label("Insert x: ");
+        label.setPrefWidth(1000.0);
+        TextField text = new TextField();
+        text.setPrefWidth(1000.0);
+        EventHandler buttonPress = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                try {
+                    width = Integer.parseInt(text.getText());
+                    initGame(stage);
+                } catch (NumberFormatException ex) {
+                    label.setText("Enter a valid Number under 20");
+                }
+            }
+        };
+        Button button = new Button("Confirm");
+        button.setOnAction(buttonPress);
+        button.setPrefWidth(1000.0);
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20));
+
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+
+        gridPane.add(label, 0, 0);
+        gridPane.add(text, 0, 1);
+        gridPane.add(button, 0, 2);
+
+        Scene scene = new Scene(gridPane, 300, 300);
+        scene.getStylesheets().add(getClass().getResource("customButton.css").toExternalForm());
+
+        stage.setScene(scene);
+
         stage.setTitle("Tic Tac Toe");
         stage.show();
-        initGame(stage, 4);
-
+        stage.setScene(scene);
     }
 
-    private void initGame(Stage stage, int width) {
+    private void bot() {
+        char[][] botField = new char[width][width];
+        for (int y = 0; y < width; y++) {
+            for (int x = 0; x < width; x++) {
+                if (field[x][y].getText().equals("")) {
+                    botField[x][y] = '\0';
+                } else {
+                    botField[x][y] = field[x][y].getText().charAt(0);
+                }
+            }
+        }
+        int[] saveBiggestRecursionScore = new int[3];
+        saveBiggestRecursionScore[0] = -100000;
+        for (int y = 0; y < width; y++) {
+            for (int x = 0; x < width; x++) {
+                if (botField[x][y] == '\0') {
+                    if (turnOfPlayer1) {
+                        botField[x][y] = 'X';
+                    } else {
+                        botField[x][y] = 'O';
+                    }
+                    int tempScore = botRecursion(botField, false);
+                    if (tempScore > saveBiggestRecursionScore[0]) {
+                        saveBiggestRecursionScore[0] = tempScore;
+                        saveBiggestRecursionScore[1] = x;
+                        saveBiggestRecursionScore[2] = y;
+                    }
+                    botField[x][y] = '\0';
+                }
+            }
+        }
+    }
+
+    private int botRecursion(char[][] botField, boolean turn) {
+        int score = 0;
+
+        return score;
+    }
+
+    private void initGame(Stage stage) {
 
         GridPane gridPane = new GridPane();
 
@@ -44,19 +115,29 @@ public class HelloApplication extends Application {
         winnerLabel.setPrefWidth(1000);
         gridPane.add(winnerLabel, 0, 0, width, 1);
 
-        Button[][] fields = new Button[width][width];
+        field = new Button[width][width];
         EventHandler buttonPress = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                if (e.getSource() instanceof Button) {
-                    Button temp = (Button) e.getSource();
-                    if (temp.getText().equals("")) {
-                        if (turnOfPlayer1) {
-                            temp.setText("X");
-                        } else {
-                            temp.setText("O");
+                if (!stop) {
+                    if (e.getSource() instanceof Button) {
+                        Button temp = (Button) e.getSource();
+                        if (temp.getText().equals("")) {
+                            if (turnOfPlayer1) {
+                                temp.setText("X");
+                                if (checkForWin()) {
+                                    winnerLabel.setText("Player X won");
+                                    stop = true;
+                                }
+                            } else {
+                                temp.setText("O");
+                                if (checkForWin()) {
+                                    winnerLabel.setText("Player O won");
+                                    stop = true;
+                                }
+                            }
+                            turnOfPlayer1 = !turnOfPlayer1;
                         }
-                        turnOfPlayer1 = !turnOfPlayer1;
                     }
                 }
             }
@@ -64,13 +145,13 @@ public class HelloApplication extends Application {
 
         for (int y = 0; y < width; y++) {
             for (int x = 0; x < width; x++) {
-                fields[x][y] = new Button();
+                field[x][y] = new Button();
                 // Allow the button to be wider overriding preferred width
-                fields[x][y].setPrefWidth(1000.0);
-                fields[x][y].setPrefHeight(1000.0);
-                fields[x][y].setOnAction(buttonPress);
+                field[x][y].setPrefWidth(1000.0);
+                field[x][y].setPrefHeight(1000.0);
+                field[x][y].setOnAction(buttonPress);
                 // set position in gridpane
-                gridPane.add(fields[x][y], x, y+1);
+                gridPane.add(field[x][y], x, y + 1);
             }
         }
 
@@ -82,11 +163,70 @@ public class HelloApplication extends Application {
 
         stage.setScene(scene);
     }
-    private boolean checkwin() {
+
+    private boolean checkForWin() {
         // returns true if someone won
+        String checkString = "";
+        boolean tempcheck = true;
+
+        // straight lines
+
+        for (int y = 0; y < width; y++) {
+            tempcheck = true;
+            checkString = field[0][y].getText();
+            for (int x = 0; x < width; x++) {
+                if (field[x][y].getText().equals("") || !field[x][y].getText().equals(checkString)) {
+                    tempcheck = false;
+                    break;
+                }
+            }
+            if (tempcheck) {
+                return true;
+            }
+        }
+        for (int x = 0; x < width; x++) {
+            tempcheck = true;
+            checkString = field[x][0].getText();
+            for (int y = 0; y < width; y++) {
+                if (field[x][y].getText().equals("") || !field[x][y].getText().equals(checkString)) {
+                    tempcheck = false;
+                    break;
+                }
+            }
+            if (tempcheck) {
+                return true;
+            }
+        }
+
+        // diagonals
+
+        tempcheck = true;
+        checkString = field[0][0].getText();
+        for (int x = 0; x < width; x++) {
+            if (field[x][x].getText().equals("") || !field[x][x].getText().equals(checkString)) {
+                tempcheck = false;
+                break;
+            }
+        }
+        if (tempcheck) {
+            return true;
+        }
+
+        tempcheck = true;
+        checkString = field[0][width - 1].getText();
+        for (int x = 0; x < width; x++) {
+            if (field[x][width - x - 1].getText().equals("") || !field[x][width - 1 - x].getText().equals(checkString)) {
+                tempcheck = false;
+                break;
+            }
+        }
+        if (tempcheck) {
+            return true;
+        }
 
         return false;
     }
+
 
     public static void main(String[] args) {
         launch();
